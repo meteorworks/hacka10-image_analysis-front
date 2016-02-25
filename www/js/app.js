81f5b454599b47123bd0f4a0aabed30339adf129
kamiyam <labs.k5m@gmail.com>
2016-02-25 13:22:06 +0900

    Add Slidebox

--- www/js/app.js
+++ www/js/app.js
@@ -1,100 +1,5 @@
 // Ionic Starter App
 
-
-(function($){
-  var $uploadCrop;
-
-  function popupResult(result) {
-
-    var html;
-    if (result.html) {
-      html = result.html;
-    }
-    if (result.src) {
-      html = '<img src="' + result.src + '" />';
-    }
-    swal({
-      title: '',
-      html: true,
-      text: html,
-      allowOutsideClick: true
-    });
-    setTimeout(function(){
-      $('.sweet-alert').css('margin', function() {
-        var top = -1 * ($(this).height() / 2),
-          left = -1 * ($(this).width() / 2);
-
-        return -150 + 'px 0 0 ' + 0 + 'px';
-      });
-    }, 1);
-  }
-
-  $(function(){
-
-      function readFile(input) {
-        if (input.files && input.files[0]) {
-          var reader = new FileReader();
-
-          reader.onload = function (e) {
-            $uploadCrop.croppie('bind', {
-              url: e.target.result
-            });
-            $('.upload-demo').addClass('ready');
-          };
-
-          reader.readAsDataURL(input.files[0]);
-        }
-        else if(!(window.File && window.FileReader && window.FileList && window.Blob)){
-          swal("Sorry - you're browser doesn't support the FileReader API");
-        }
-      }
-
-      $uploadCrop = $('#upload-demo').croppie({
-        viewport: {
-          width: 200,
-          height: 200,
-          type: 'circle'
-        },
-        boundary: {
-          width: 320,
-          height: 320
-        },
-        exif: true
-      });
-
-      $('#upload').on('change', function(){
-        readFile(this);
-      });
-
-      $('.upload-result').on('click', function (ev) {
-
-        $uploadCrop.croppie('result', {
-          type: 'canvas',
-          size: 'viewport'
-        }).then(function (data) {
-
-
-          return  popupResult({
-            html: "<h1>スコア: " + 10 + "</h1>"
-          });
-
-          socket.post('/question/', {
-            id: 1,
-            answer: data
-          }, function (resData) {
-            console.log(resData);
-            popupResult({
-              html: "<h1>スコア: " + resData.score + "</h1>"
-            });
-          });
-        });
-      });
-
-  })
-})(jQuery);
-
-
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -129,57 +34,57 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // setup an abstract state for the tabs directive
     .state('dash', {
       url: '/dash',
-      abstract: true,
+      abstract: false,
       templateUrl: 'templates/dash.html'
-    })
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    });
+  //
+  //// setup an abstract state for the tabs directive
+  //  .state('tab', {
+  //  url: '/tab',
+  //  abstract: true,
+  //  templateUrl: 'templates/tabs.html'
+  //})
+  //
+  //// Each tab has its own nav history stack:
+  //
+  //.state('tab.dash', {
+  //  url: '/dash',
+  //  views: {
+  //    'tab-dash': {
+  //      templateUrl: 'templates/tab-dash.html',
+  //      controller: 'DashCtrl'
+  //    }
+  //  }
+  //})
+  //
+  //.state('tab.chats', {
+  //    url: '/chats',
+  //    views: {
+  //      'tab-chats': {
+  //        templateUrl: 'templates/tab-chats.html',
+  //        controller: 'ChatsCtrl'
+  //      }
+  //    }
+  //  })
+  //  .state('tab.chat-detail', {
+  //    url: '/chats/:chatId',
+  //    views: {
+  //      'tab-chats': {
+  //        templateUrl: 'templates/chat-detail.html',
+  //        controller: 'ChatDetailCtrl'
+  //      }
+  //    }
+  //  })
+  //
+  //.state('tab.account', {
+  //  url: '/account',
+  //  views: {
+  //    'tab-account': {
+  //      templateUrl: 'templates/tab-account.html',
+  //      controller: 'AccountCtrl'
+  //    }
+  //  }
+  //});
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/dash');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,105 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicSlideBoxDelegate) {
+
+  $scope.slideHasChanged = function(index){
+    if(index == 3 ){
+      console.log(index);
+
+      $ionicSlideBoxDelegate.enableSlide(false)
+    }
+  }
+
+  var $uploadCrop;
+
+  function popupResult(result) {
+
+    var html;
+    if (result.html) {
+      html = result.html;
+    }
+    if (result.src) {
+      html = '<img src="' + result.src + '" />';
+    }
+    swal({
+      title: '',
+      html: true,
+      text: html,
+      allowOutsideClick: true
+    });
+    setTimeout(function(){
+      $('.sweet-alert').css('margin', function() {
+        var top = -1 * ($(this).height() / 2),
+          left = -1 * ($(this).width() / 2);
+
+        return -150 + 'px 0 0 ' + 0 + 'px';
+      });
+    }, 1);
+  }
+
+  $(function(){
+
+    function readFile(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+          $uploadCrop.croppie('bind', {
+            url: e.target.result
+          });
+          $('.upload-demo').addClass('ready');
+        };
+
+        reader.readAsDataURL(input.files[0]);
+      }
+      else if(!(window.File && window.FileReader && window.FileList && window.Blob)){
+        swal("Sorry - you're browser doesn't support the FileReader API");
+      }
+    }
+
+    $uploadCrop = $('#upload-demo').croppie({
+      viewport: {
+        width: 200,
+        height: 200,
+        type: 'circle'
+      },
+      boundary: {
+        width: 320,
+        height: 320
+      },
+      exif: true
+    });
+
+    $('#upload').on('change', function(){
+      readFile(this);
+    });
+
+    $('.upload-result').on('click', function (ev) {
+
+      $uploadCrop.croppie('result', {
+        type: 'canvas',
+        size: 'viewport'
+      }).then(function (data) {
+
+
+        return  popupResult({
+          html: "<h1>スコア: " + 10 + "</h1>"
+        });
+
+        socket.post('/question/', {
+          id: 1,
+          answer: data
+        }, function (resData) {
+          console.log(resData);
+          popupResult({
+            html: "<h1>スコア: " + resData.score + "</h1>"
+          });
+        });
+      });
+    });
+
+  });
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
